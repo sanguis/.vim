50f2afeae5bfef6ebfb4d8dd4bb36afb83f31354
Josh Beauregard <sanguis@users.noreply.github.com>
2022-01-19 15:58:34 -0500

    Only running plugin functions if the amount of functions change

--- vim-shell-utils.zsh
+++ vim-shell-utils.zsh
@@ -2,12 +2,17 @@
 export VIMRC=$HOME/.sanguis_settings/vim/vimrc
 
 
-## refrenced by zshrc, contains funtions for vimconfig editing
+## referenced by zshrc, contains functions for vim config editing
 # edit and commit changed to vimrc
 vimrc_edit() {
+
+  local pre_count=$(grep -cE '^\s+Plug' $VIMRC)
+  [[ $DEBUG ]] && echo -e "\033[34;1m[DEBUG]\033[0m Before editing there are $pre_count plugins listed referenced"
   vim $VIMRC
   git -C $HOME/.sanguis_settings/vim commit $VIMRC
-  vim +PlugInstall +PlugUpdate +PlugClean +qall
+  local post_count=$(grep -cE '^\s+Plug' $VIMRC)
+  [[ $DEBUG ]] && echo -e "\033[34;1m[DEBUG]\033[0m After editing there are $post_count plugins listed referenced"
+  [[ $post_count != $pre_count ]] && vim +PlugInstall +PlugUpdate +PlugClean +qall
 }
 
 vim_plug_update() {
